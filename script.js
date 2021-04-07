cc3dd8b40e30f8d70698f5bf227c8167a9f7f232
unknown <rfasulu@hotmail.com>
2021-04-07 13:44:06 +0200

    jQuery-Countries

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function getCountries() {
-    
-})
+}
+
+$(function () {
+    $("#btnShowCountries").click(function () {
+        $.ajax(
+            {
+                url: "http://location:8000/countries",
+        
+                success: function (data, status, response) {
+                    console.log(data);
+
+                    for(var i=0;i< data.length;i++){
+                        $("ul").append(`<li>${data[i]}</li>`)
+                    }
+                }
+            }
+        );
+        // var listCountries = getCountries();
+        // console.log(listCountries);
+    })
+});
